2bdc28dfaadd650750909971180d7218dee64f0a
SudeepMishra <sudeepm0599@gmail.com>
2024-05-07 12:15:27 +0545

    changes

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,49 +7,54 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index(){
-        $category= Category::all();
+    public function index()
+    {
+        $category = Category::all();
         return view('category.index', compact("category"));
     }
 
-    public function create(){
+    public function create()
+    {
         return view('category.create');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $slug = $request->name;
-        $category= Category::create([
-            'name'=> $request->name,
-            'slug'=> $slug
+        $category = Category::create([
+            'name' => $request->name,
+            'slug' => $slug
         ]);
 
-        return redirect()->route('category')->with('message','category added');
-        }
-    
-        public function edit(Request $request, $id){
-            $item = Category::findOrFail($id);
-            $item->update($request->all());
-            return view('category.edit', compact('item'));
-        }
+        return redirect()->route('category')->with('message', 'category added');
+    }
 
-        public function update(Request $request, $id)
-        {
-            $validatedData = $request->validate([
-                'name' => 'required|string|max:255',
-            ]);
-        
-            $role = Category::findOrFail($id);
-            $role->update($validatedData);
-        
-            return redirect()->route('category.edit', $role->id)->with('success', 'Role updated successfully');
+    public function edit(Request $request, $id)
+    {
+        $item = Category::findOrFail($id);
+        $item->update($request->all());
+        return view('category.edit', compact('item'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $role = Category::findOrFail($id);
+        $role->update($validatedData);
+
+        return redirect()->route('category.edit', $role->id)->with('success', 'Role updated successfully');
+    }
+    public function destroy($id)
+    {
+        $category = Category::findOrFail($id);
+        if ($category) {
+            $category->delete();
+            return redirect()->route('category')->with('success', 'Category deleted successfully');
+        } else {
+            return redirect()->route('category')->with('error', 'Not found');
         }
-        public function destroy($id)
-{
-    $category = Category::findOrFail($id);
-    $category->delete();
-    
-    return redirect()->route('category.index')->with('success', 'Category deleted successfully');
-}
-
-
+    }
 }
--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,51 +1,47 @@
 @extends('layouts.master')
 
 @section('title')
-Dashboard | Funda of Web IT
+    Dashboard | Funda of Web IT
 @endsection
 
 @section('content')
+    <div class="row mt-5">
+        <a href="/home/category/add">Add category</a>
+        <div class="col-12">
+            <h2> All category</h2>
+        </div>
+        <div class="col-12">
+            <table class="table table-reponsive">
+                <thead>
+                    <tr>
+                        <th>SN</th>
+                        <th>Name</th>
+                        <th>Slug</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($category as $item)
+                        <tr>
+                            <td>{{ $item->id }}</td>
+                            <td>{{ $item->name }}</td>
+                            <td>{{ $item->slug }}</td>
+                            <td>
+                                <div class="d-flex">
+                                    <a href="/role-edit/{{ $item->id }}" class="btn btn-sucess">Edit</a>
 
-<div class="row mt-5">
-    <a href="/home/category/add">Add category</a>
-    <div class="col-12">
-    <h2> All category</h2>
+                                    <form action="{{ route('category.destroy', $item->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+        </div>
     </div>
-    <div class="col-12">
-        <table class="table table-reponsive" >
-            <thead>
-                <tr>
-                    <th>SN</th>
-                    <th>Name</th>
-                    <th>Slug</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-            @foreach ( $category as $item )
-            <tr>
-                <td>{{$item->id}}</td>
-                <td>{{$item->name}}</td>
-                <td>{{$item->slug}}</td>
-                <td>
-                    <div class="d-flex">
-                    <a href="/role-edit/{{$item->id}}" class="btn btn-sucess">Edit</a>
-
-                    <form action="{{ route('category.delete', $item->id) }}" method="POST">
-    @csrf
-    @method('DELETE')
-    <button type="submit" class="btn btn-danger">Delete</button>
-</form>
-                    </div>
-                </td>
-            </tr>
-    @endforeach
-            </tbody>
-        </table>
-
-    </div>
-</div>
-
-
-
 @endsection
